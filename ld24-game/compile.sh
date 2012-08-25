74eabfbe5d9176bb079ad4b9bb668b96d7709fa2
Daniel Kurashige-Gollub <daniel@kurashige-gollub.de>
2012-08-25 14:13:55 +0900

    moved generated .js files into separate folder, that is not part of the repository

--- ld24-game/compile.sh
+++ ld24-game/compile.sh
@@ -7,6 +7,13 @@ fi
 
 DART="$DART_SDK/bin/dart2js"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+GENERATED="$DIR/generated"
+
+if [ ! -d "$GENERATED" ]; then
+    mkdir -p $GENERATED
+    [[ ! $? -eq 0 ]] && echo "Could not create output folder $GENERATED. Aborted." && exit 2
+fi
+
 
 for file in `ls $DIR/*.dart`
 do
@@ -16,8 +23,9 @@ do
         filename="${filename%.*}"
         output="$filename.js"
         echo "Compilling $filename.dart to $output ..."
-        $DART -o$output $file
-        rm $output.map
+        ofile="$GENERATED/$output"
+        $DART -o$ofile $file
+        rm $ofile.map
     fi
 done
 
